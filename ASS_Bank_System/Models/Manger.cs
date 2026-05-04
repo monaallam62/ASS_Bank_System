12e79695696ad5efc2f42e40770eb0dcad7e965c
monaallam62 <monaallam5102@gmail.com>
2026-05-05 00:18:28 +0300

    Manager

---
+++ ASS_Bank_System/Models/Manger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_Bank_System.Models
+{
+    public class Manger
+    {
+        public int MangerId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime HireDate { get; set; }
+
+        public Branch Branchs { get; set; }
+    }
+}
