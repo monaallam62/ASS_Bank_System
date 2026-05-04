1faa27409f2c5985cb19100bb93141a6304c7f40
monaallam62 <monaallam5102@gmail.com>
2026-05-04 20:59:15 +0300

    Add project files.

---
+++ ASS_Bank_System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ASS_Bank_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
