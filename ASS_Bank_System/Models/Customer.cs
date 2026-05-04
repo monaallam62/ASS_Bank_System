6dda6e764d1d837e438e7780e630903802908619
monaallam62 <monaallam5102@gmail.com>
2026-05-05 00:19:23 +0300

    Customer

---
+++ ASS_Bank_System/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_Bank_System.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string NationalId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string CustomerType { get; set; }
+
+        public ICollection<CustomerAccount>? CustomerAccounts { get; set; }
+    }
+}
